353fccc5e49b588d860f7d8073e1b2167b43ad96
luismancor <juan.mancilla.c@tecsup.edu.pe>
2020-06-21 12:21:12 -0700

    Initial Commit

---
+++ Lab10SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab10SoundBoard
+//
+//  Created by Juan Mancilla on 6/21/20.
+//  Copyright © 2020 Juan Mancilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
